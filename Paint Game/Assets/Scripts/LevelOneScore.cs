f2aa40fabe69b17e5f95b8f364d5c32e94ef230d
kmsalzman <kmsalzman@gmail.com>
2021-06-07 01:34:05 -0400

    Everything Except Leaving a Trail of Paint

--- Paint Game/Assets/Scripts/LevelOneScore.cs
+++ Paint Game/Assets/Scripts/LevelOneScore.cs
@@ -1,24 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelOneScore : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject overlay;
+    public GameObject star1;
+    public GameObject star2;
+    public GameObject star3;
+    public GameObject playerOne;
+    public GameObject playerTwo;
+    public GameObject lockedDoor;
+    public GameObject lockedDoor2;
+    public int goalNum;
+    public int goalTotal;
+    public int paintedNum;
+    public float tileNum;
+    public float score;
     PlayerController controllerScript;
+    Scene currentScene;
+
     // Start is called before the first frame update
     void Start()
     {
-        controllerScript = player.GetComponent<PlayerController>();
-        controllerScript.currentPostion = new Vector3(-34.1105f, -16.0905f, 90f);
-        player.transform.position = controllerScript.currentPostion;
-        controllerScript.tileNum = 9;
-        controllerScript.paintedNum = 0;
+        currentScene = SceneManager.GetActiveScene();
+        controllerScript = playerOne.GetComponent<PlayerController>();
+        if (currentScene.name == "Level1")
+        {
+            controllerScript.startingOne = new Vector3(-34.1105f, -16.0905f, 90f);
+            tileNum = 10f;
+            goalTotal = 1;
+        }
+        else if (currentScene.name == "Level2")
+        {
+            controllerScript.startingOne = new Vector3(-16.7136f, -33.0435f, 90f);
+            controllerScript.startingTwo = new Vector3(17f, -33.0435f, 90f);
+            tileNum = 16f;
+            goalTotal = 2;
+        }
+        else if (currentScene.name == "Level3")
+        {
+            controllerScript.startingOne = new Vector3(-16.2f, -32.9f, 90f);
+            controllerScript.startingTwo = new Vector3(17f, -33f, 90f);
+            tileNum = 9f;
+            goalTotal = 2;
+        }
+        else if (currentScene.name == "Level4")
+        {
+            controllerScript.startingOne = new Vector3(-51.07925f, -4.266584f, 90f);
+            controllerScript.startingTwo = new Vector3(51.5f, -4.5f, 90f);
+            tileNum = 24f;
+            goalTotal = 2;
+        }
+        else if (currentScene.name == "Level5")
+        {
+            controllerScript.startingOne = new Vector3(-8.4637f, -32.9892f, 90f);
+            controllerScript.startingTwo = new Vector3(8.2f, -32.8f, 90f);
+            tileNum = 14f;
+            goalTotal = 2;
+        }
+        else if (currentScene.name == "Level6")
+        {
+            controllerScript.startingOne = new Vector3(-51.07925f, 12.5f, 90f);
+            controllerScript.startingTwo = new Vector3(51.5f, 12.5f, 90f);
+            tileNum = 28f;
+            goalTotal = 2;
+        }
+        
+        playerOne.transform.position = controllerScript.startingOne;
+        playerTwo.transform.position = controllerScript.startingTwo;
+        
+        paintedNum = 0;
+        overlay.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void victory(int paintedTiles, int goal)
+    {
+        if (goal == goalTotal)
+        {
+            score = paintedTiles / tileNum * 100f;
+
+            if (score < 33)
+            {
+                star1.SetActive(false);
+                star2.SetActive(false);
+                star3.SetActive(false);
+            }
+            else if (score > 33 && score < 66)
+            {
+                star1.SetActive(true);
+                star2.SetActive(false);
+                star3.SetActive(false);
+            }
+            else if (score > 66 && score < 100)
+            {
+                star1.SetActive(true);
+                star2.SetActive(false);
+                star3.SetActive(true);
+            }
+            else
+            {
+                star1.SetActive(true);
+                star2.SetActive(true);
+                star3.SetActive(true);
+            }
+            Debug.Log("You won! Your score is: " + score + "% tileNum: " + tileNum + " paintedTiles: " + paintedTiles);
+            overlay.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Not Enough Goal " + goal + "Goal total: " + goalTotal);
+        }
     }
 }
--- Paint Game/Assets/Scripts/PlayerController.cs
+++ Paint Game/Assets/Scripts/PlayerController.cs
@@ -2,46 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-    public int tileNum;
-    public int paintedNum;
     public float score;
     public Vector3 currentPostion;
     private Vector3 previousPosition;
+    public Vector3 startingOne;
+    public Vector3 startingTwo;
+    public GameObject levelScript;
+    public Image tileImage;
+    public Sprite playerOnePaint;
+    public Sprite playerTwoPaint;
+    LevelOneScore leveloneScript;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        leveloneScript = levelScript.GetComponent <LevelOneScore>();
+        playerOnePaint = Resources.Load<Sprite>("Blue Paint");
+        playerTwoPaint = Resources.Load<Sprite>("Pink Paint");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("d"))
+        if (this.name == "PlayerOne")
         {
-            previousPosition = transform.position;
-            transform.Translate(Vector3.right * speed, Space.World);
-        }
-        else if (Input.GetKeyDown("a"))
+            if (Input.GetKeyDown("d"))
+            {
+                previousPosition = transform.position;
+                transform.Translate(Vector3.right * speed, Space.World);
+            }
+            else if (Input.GetKeyDown("a"))
+            {
+                previousPosition = transform.position;
+                transform.Translate(Vector3.left * speed, Space.World);
+            }
+            else if (Input.GetKeyDown("w"))
+            {
+                previousPosition = transform.position;
+                transform.Translate(Vector3.up * speed, Space.World);
+            }
+            else if (Input.GetKeyDown("s"))
+            {
+                previousPosition = transform.position;
+                transform.Translate(Vector3.down * speed, Space.World);
+            }
+            
+        }    
+        else if (this.name == "PlayerTwo")
         {
-            previousPosition = transform.position;
-            transform.Translate(Vector3.left * speed, Space.World);
+            if (Input.GetKeyDown("right"))
+            {
+                previousPosition = transform.position;
+                transform.Translate(Vector3.right * speed, Space.World);
+            }
+            else if (Input.GetKeyDown("left"))
+            {
+                previousPosition = transform.position;
+                transform.Translate(Vector3.left * speed, Space.World);
+            }
+            else if (Input.GetKeyDown("up"))
+            {
+                previousPosition = transform.position;
+                transform.Translate(Vector3.up * speed, Space.World);
+            }
+            else if (Input.GetKeyDown("down"))
+            {
+                previousPosition = transform.position;
+                transform.Translate(Vector3.down * speed, Space.World);
+            }
         }
-        else if (Input.GetKeyDown("w"))
-        {
-            previousPosition = transform.position;
-            transform.Translate(Vector3.up * speed, Space.World);
-        }
-        else if (Input.GetKeyDown("s"))
-        {
-            previousPosition = transform.position;
-            transform.Translate(Vector3.down * speed, Space.World);
-        }
-        else if (Input.GetKeyDown("r"))
+
+        if (Input.GetKeyDown("r"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
@@ -52,19 +89,45 @@ public class PlayerController : MonoBehaviour
         if (collision.tag == "Ground" || collision.tag == "Start")
         {
             collision.tag = "Painted Ground";
-            paintedNum++;
-            Debug.Log("You painted the ground!");
+            if (this.gameObject.tag == "PlayerOne")
+            {
+                collision.GetComponent<Image>().sprite = playerOnePaint;
+                Debug.Log(tileImage);
+            }
+            else if (this.gameObject.tag == "PlayerTwo")
+            {
+                collision.GetComponent<Image>().sprite = playerTwoPaint;
+                Debug.Log(tileImage);
+            }
+            
+            leveloneScript.paintedNum++;
+            Debug.Log("You painted the ground! Painted " + leveloneScript.paintedNum + " tiles!");
+        }
+        else if (collision.tag == "Button")
+        {
+            leveloneScript.lockedDoor.tag = "Ground";
+            leveloneScript.lockedDoor2.tag = "Ground";
+            leveloneScript.paintedNum++;
         }
         else if (collision.tag == "End")
         {
-            score = (paintedNum / tileNum) * 100;
-            Debug.Log("You won! Your score is: " + score + "%");
-            SceneManager.LoadScene("GameOver");
+            collision.tag = "Boundary";
+            leveloneScript.goalNum++;
+            leveloneScript.victory(leveloneScript.paintedNum, leveloneScript.goalNum);
         }
         else
         {
             Debug.Log("You can't step there!");
             transform.position = previousPosition;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Button")
+        {
+            leveloneScript.lockedDoor.tag = "Locked";
+            leveloneScript.lockedDoor2.tag = "Locked";
         }
     }
 }
